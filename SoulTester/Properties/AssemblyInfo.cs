5616412578dde498290371225a956017a51a914a
Bluegent <bluegentleman@live.com>
2019-11-08 14:34:32 +0200

    Added skelton test and library project

---
+++ SoulTester/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SoulTester")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Elektrobit Automotive GmbH")]
+[assembly: AssemblyProduct("SoulTester")]
+[assembly: AssemblyCopyright("Copyright © Elektrobit Automotive GmbH 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b2769b26-7ea8-4eef-9c3f-64c9d057f341")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
