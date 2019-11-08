9dcd08f9d3a252303a9e0b8314855e11ec6f1682
Bluegent <bluegentleman@live.com>
2019-11-08 15:22:27 +0200

    Added some classes for stat management

---
+++ SoulCollector/Entities/Stat.cs
@@ -0,0 +1,51 @@
+﻿namespace SoulCollector.Entities
+{
+    using System.Collections.Generic;
+
+    public enum StatType
+    {
+        Attack, //flat damage add
+        Defense, //flat damage reduction
+        Critical, //chance to do a critical strike
+        CriticalMultiplier, //damage multiplier for criticals
+        DodgeChance, //chance to dodge attacks
+
+    }
+
+    public class Stat
+    {
+        public StatType Type { get; private set; }
+        private int _base;
+
+        public int Value { get; private set; }
+        private List<StatModifier> _modifiers;
+
+        public Stat(StatType type, int baseValue)
+        {
+            _modifiers = new List<StatModifier>();
+            Type = type;
+            _base = baseValue;
+            Recalculate();
+        }
+
+        private void Recalculate()
+        {
+            Value = _base;
+            foreach (StatModifier mod in _modifiers)
+            {
+                Value += mod.GetValue(_base);
+            }
+        }
+
+        public void AddModifier(StatModifier mod)
+        {
+            _modifiers.Add(mod);
+            Recalculate();
+        }
+
+        public void RemoveModifier(StatModifier mod)
+        {
+            _modifiers.Remove(mod);
+        }
+    }
+}
---
+++ SoulCollector/Entities/StatModifier.cs
@@ -0,0 +1,34 @@
+﻿namespace SoulCollector.Entities
+{
+
+    public enum ModifierType
+    {
+        Flat,
+        Percentage
+    }
+
+    public class StatModifier
+    {
+        private ModifierType _type;
+        private int _value;
+
+        public StatModifier(ModifierType type, int value)
+        {
+            _value = value;
+            _type = type;
+        }
+
+        public int GetValue(int baseValue)
+        {
+            switch (_type)
+            {
+                case ModifierType.Flat:
+                    return _value;
+                case ModifierType.Percentage:
+                    return (int)(_value / 100.0f * baseValue);
+                default:
+                    return 0;
+            }
+        }
+    }
+}
