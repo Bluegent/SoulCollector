68217059826418bab95fac8858b80adb8fadd2dc
Bluegent <bluegentleman@live.com>
2019-11-10 13:14:12 +0200

    Added skeleton code for Entity

---
+++ SoulCollector/Entities/Entity.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace SoulCollector.Entities
+{
+    public class BaseEntity
+    {
+        protected Dictionary<StatType, Stat> Stats;
+        protected Dictionary<ResourceType, Resource> Resources;
+
+        public BaseEntity()
+        {
+            Stats= new Dictionary<StatType, Stat>();
+            Resources = new Dictionary<ResourceType, Resource>();
+            Resource health = new Resource(ResourceType.Health, 100, 100);
+            Resources.Add(health.Type,health);
+        }
+
+    }
+}
